ac5df2155a6a466e4f82e2dcdce787d1f8cfed55
krushna gurav <krushna.gurav@differenzsystem.com>
2022-05-04 08:36:04 +0530

    add  privacy and terms pages

--- src/Helper/Full.js
+++ src/Helper/Full.js
@@ -9,6 +9,8 @@ import ProfileContainer from "../Containers/Profile";
 import RegisterContainer from "../Containers/Register";
 import ResetPasswordContainer from "../Containers/ResetPassword";
 import verifyEmailContainer from "../Containers/verifyEmail";
+import PrivacyPolicy from "../Pages/PrivacyPolicy";
+import TermsCondition from "../Pages/TermsCondition";
 import { getToken } from "./LocalStorage";
 import Main from "./Main";
 import routes from "./routes";
@@ -71,6 +73,18 @@ const Full = (props) => {
           name="profile"
           path={routes.profile}
           component={ProfileContainer}
+        />
+        <Route
+          exact
+          path={routes.terms}
+          name="terms-condition"
+          component={TermsCondition}
+        />
+        <Route
+          exact
+          path={routes.policy}
+          name="privacy-policy"
+          component={PrivacyPolicy}
         />
         <Redirect from="/" to={routes.home} />
         {/* <Route path="*" exact component={PageNotFound} /> */}
