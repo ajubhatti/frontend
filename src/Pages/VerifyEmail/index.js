68d9130028ac1db10f212f7aca488287187d23fe
krushna gurav <krushna.gurav@differenzsystem.com>
2022-05-14 13:19:28 +0530

    create varify account design

--- src/Pages/VerifyEmail/index.js
+++ src/Pages/VerifyEmail/index.js
@@ -1,28 +1,43 @@
 import React from "react";
-import VectorImg from "../../Assets/vector.png";
+import { toast } from "react-toastify";
+import iconChecked from "../../Assets/iconChecked.svg";
+import routes from "../../Helper/routes";
 
 const VerifyEmail = (props) => {
+
+  const clickHandler = async () => {
+    // await props.verify().then((res) => {
+    //   toast.success(res.message);
+    //   props.history.push(routes.home);
+    // });
+  };
   return (
-    <div className="space-2">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-6">
-            <div className="content-box">
-              <h2>Verify Account</h2>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit sedo
-              </p>
-            </div>
-            {/* content here */}
+    <>
+      <div className="container space-2 space-lg-3">
+        <div className="w-md-80 w-lg-50 text-center mx-md-auto">
+          <figure
+            id="iconChecked"
+            className="ie-height-90 max-width-11 mx-auto mb-3"
+          >
+            <img src={iconChecked} alt="" />
+          </figure>
+          <div className="mb-5">
+            <h1 className="h3 font-weight-medium">Welcome!</h1>
+            <p>
+              We're excited to have you get started. First, you need conform
+              your account. just press the button below.
+            </p>
           </div>
-          <div className="col-md-6">
-            <div className="vector-img">
-              <img src={VectorImg} alt="" className="w-100" />
-            </div>
-          </div>
+          <button
+            className="btn btn-primary btn-pill transition-3d-hover px-5"
+            onClick={clickHandler}
+          >
+            Conform Account
+          </button>
         </div>
       </div>
-    </div>
+      <hr className="my-0" />
+    </>
   );
 };
 
